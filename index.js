9d1eb85ebeb219b04d06bf50e67d8f1d44eac3ca
bre10000 <burukaddis@gmail.com>
2020-02-28 18:23:19 +0300

    Added hello world response

--- index.js
+++ index.js
@@ -1614,9 +1614,16 @@ function employerAction(ctx) {
 
     }
 }
+var server = express();
+
 
 var server_port = process.env.PORT || 80;
 var server_host = '0.0.0.0';
 server.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
+
+
+server.get("/", fucntion (req,res) {
+           res.send("Hello World");
+           });
